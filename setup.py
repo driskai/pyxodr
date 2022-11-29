5f71921773f0553650ec7df7cd0cae953a1914c0
HughBlayney <hugh.blayney@gmail.com>
2022-11-29 16:00:11 +0000

    Minimal setuptools to support editable install

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
-requires = [
-    "lxml>=4.9.1",
-    "matplotlib>=3.5.0",
-    "numpy>=1.21.0",
-    "rich>=12.6.0",
-    "scipy>=1.7.0",
-    "Shapely>=1.8.4",
-]
-
-extras = {"dev": ["pytest>=7.1.3"]}
-
-setup(
-    author="Hugh Blayney",
-    author_email="hugh@drisk.ai",
-    description="Read OpenDRIVE files.",
-    install_requires=requires,
-    extras_require=extras,
-    name="pyxodr",
-    version="0.1.0",
-    packages=find_packages(
-        where=".",
-        include=["pyxodr", "pyxodr.*"],
-    ),
-)
+if __name__ == "__main__":
+    setuptools.setup()
